9a36eb4054c1e2f694a6dbff10492589ddc37c77
masterdev1013 <masterdev1013@gmail.com>
2023-11-01 21:22:24 -0700

    thirdnextjs

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Head from 'next/head';
 import Link from 'next/link';
 export default function AboutPage() {
     return (
         <div>
+            <Head>
+                <title>About</title>
+                <meta name="description" content="You really need to read"/>
+            </Head>
             <h1>About Page</h1>
             <Link href={{pathname:'/blog/[slug]' , query:{slug:'123'}}}>Blog</Link>
         </div>
