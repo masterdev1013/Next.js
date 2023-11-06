384169cb193f4b37fd3cf85b27f8c76eb0bc7a85
masterdev1013 <masterdev1013@gmail.com>
2023-11-05 23:42:09 -0800

    complete

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,23 @@
 import Head from 'next/head';
 import Link from 'next/link';
-export default function AboutPage() {
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+export default function About() {
+
+    const {data , error} = useSWR("api/about" , fetcher);
+
+    if(error) return <p>Error fetching data...</p>
+    if(!data) return <p>Loading...</p>
     return (
         <div>
             <Head>
                 <title>About</title>
                 <meta name="description" content="You really need to read"/>
             </Head>
-            <h1>About Page</h1>
-            <Link href={{pathname:'/blog/[slug]' , query:{slug:'123'}}}>Blog</Link>
+            
+            <p>My name is {data.name}</p>
         </div>
     )
 }; 
--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,30 @@
-import { useRouter } from "next/router"
+// import { useRouter } from "next/router"
+import Head from "next/head";
 
-export default function BlogPage() {
+export default function Blog({name}) {
 
-    const router = useRouter();
+    // const router = useRouter();
     return (
         <div>
-            <h1>Blog Page</h1>
-            <button onClick={() => {router.push('/about')}}>About</button>
+            <Head>
+                <title>Blog</title>
+                <meta name="description" content="You reall need to read"/>
+            </Head>
+            {/* <h1>Blog Page</h1>
+            <button onClick={() => {router.push('/about')}}>About</button> */}
+
+            <p>My name is {name}</p>
         </div>
     )
 };
+
+
+export async function getServerSideProps() {
+  const data = await fetch("https://randomuser.me/api").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: { name: data.results[0].name.first },
+  };
+}
